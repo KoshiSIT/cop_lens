ec5def7e022b6709acf69343e3d77915e4cf75ec
叶 恒志 <k_yo@MacBook-Pro.local>
2025-10-12 18:13:50 +0900

    fix: EMA.jsをグローバルスコープに公開してDevToolsフックからアクセス可能に

--- examples/remote-editor-web/public/js/ema/loader.js
+++ examples/remote-editor-web/public/js/ema/loader.js
@@ -4,5 +4,11 @@ import Layer from './Layer.js';
 import Signal from './Signal.js';
 import SignalComp from './SignalComp.js';
 
+// Export for ES Modules
 export { EMA, Layer, Signal, SignalComp };
 export const show = console.log;
+
+// Also expose globally for DevTools hook
+window.EMA = EMA;
+window.Layer = Layer;
+window.Signal = Signal;
